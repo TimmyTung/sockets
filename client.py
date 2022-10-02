c549c05f16ea5ab23f3facae846909512c0257e0
TimmyTung <74445389+TimmyTung@users.noreply.github.com>
2022-10-02 16:25:39 -0700

    Added user inputs and command line input

--- client.py
+++ client.py
@@ -1,21 +1,78 @@
 import socket
+import sys
 
-UDP_IP = "Timmys-MacBook-Pro.local"
-UDP_PORT = 5005
-msg = b"register @a 127.23.12.34 23423 93423 21321"
-#msg = b"register @b 001.22.33.44 334 432 654"
-#msg = b"register @c 999.99.99.99 dsf ffd fds"
-#msg = b"query handles"
-#msg = b"follow @c @b"
-#msg = b"follow @a @b"
-#msg = b"print"
-#msg = b"drop @a @b"
-#msg = b"exit @b"
-#msg = b"print"
+# allow user to pass in server IP
+if(len(sys.argv) != 3):
+    print("need both server IP and port number")
+    exit(0)
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(msg, (UDP_IP, UDP_PORT))
+UDP_IP = sys.argv[1] #input("please enter server IP\n") # Timmys-MacBook-Pro.local
+UDP_PORT = int(sys.argv[2])
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# have user make a handle name and go ahead and register it
+handle = input("enter a handle (without the @)\n")
+
+port1 = input("enter port number 1")
+port2 = input("enter port number 2")
+port3 = input("enter port number 3")
+hostname = socket.gethostname()
+IP = socket.gethostbyname(hostname)
+
+sock.sendto(f"register @{handle} {IP} {port1} {port2} {port3}".encode("utf-8"), (UDP_IP, UDP_PORT))
+
 reply, addr = sock.recvfrom(1024)
 reply = reply.decode("utf-8")
+
 print("received message: " + reply)
+
+replyArray = reply.split()
+if(replyArray[1] == "FAILURE"):
+    print("please restart with a unique handle")
+    exit(0)
+
+msg = ""
+userIn = ""
+
+""""
+msg = b"register @a 127.23.12.34 23423 93423 21321"
+msg = b"register @b 001.22.33.44 334 432 654"
+msg = b"register @c 999.99.99.99 dsf ffd fds"
+msg = b"query handles"
+msg = b"follow @c @b"
+msg = b"follow @a @b"
+msg = b"print"
+msg = b"drop @a @b"
+msg = b"print"
+msg = b"exit @b"
+msg = b"print"
+"""
+
+# allow user to pass in command
+while(userIn != "exit") :
+    userIn = input("please enter a command\n")
+    inputs = userIn.split()
+
+    if(inputs[0] == "follow" and len(inputs) == 2):
+        msg = f"follow @{handle} {inputs[1]}".encode("utf-8")
+
+    elif(inputs[0] == "drop" and len(inputs) == 2):
+        print(inputs[1])
+        msg = f"drop @{handle} {inputs[1]}".encode("utf-8")
+
+    elif (inputs[0] == "exit" and len(inputs) == 1):
+        msg = f"exit @{handle}".encode("utf-8")
+
+    elif (inputs[0] == "query"):
+        msg = f"query handles".encode("utf-8")
+
+    else:
+        print("unknown command or poorly formated command")
+        continue
+
+    sock.sendto(msg, (UDP_IP, UDP_PORT))
+
+    reply, addr = sock.recvfrom(1024)
+    reply = reply.decode("utf-8")
+
+    print("received message: " + reply)
--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 import socket
+import sys
 
-UDP_IP = socket.gethostname()
-print(UDP_IP)
-UDP_PORT = 5005
+if(len(sys.argv) != 2):
+    print("need port number")
+    exit(0)
 
-sock = socket.socket(socket.AF_INET,  # Internet
-                     socket.SOCK_DGRAM)  # UDP
-sock.bind((UDP_IP, UDP_PORT))
+hostname = socket.gethostname()
+UDP_IP = socket.gethostbyname(hostname)
+UDP_PORT = int(sys.argv[1])
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("0.0.0.0", UDP_PORT))
+
+print("server is running")
 
 users = dict()
 followers = dict()
@@ -19,6 +25,10 @@ def register(inputs):
     port1 = inputs[3]
     port2 = inputs[4]
     port3 = inputs[5]
+
+    if(handle in users):
+        print("duplicate user " + handle)
+        return ("FAILURE @" + handle + " is not registered").encode("utf-8")
 
     print("user" + handle + "registers" + IP + port1 + port2 + port3)
 
@@ -34,12 +44,12 @@ def register(inputs):
     for key in users:
         print(key, users[key])
 
-    output = "@" + handle + " is registered"
+    output = "SUCCESS @" + handle + " is registered"
     return output.encode("utf-8")
 def query():
     # query handles
     # get number and list of all users
-    output = str(len(users)) + " "
+    output = "SUCCESS " + str(len(users)) + " "
     for key in users:
         output += key + " "
 
@@ -49,33 +59,68 @@ def query():
 def follow(input):
     # follow @<handle1> @<handle2>
     # add handle 1 to list of handle 2's followers and keep that list sorted
+    # add handle 2 to lest of handle 1's follows
     handle1 = inputs[1][1:]
     handle2 = inputs[2][1:]
+
+    if(handle1 == handle2):
+        print("cannot follow itself")
+        return ("FAILURE @" + handle1 + " could not follow itself").encode("utf-8")
+
+    if(not handle2 in users):
+        print("user @" + handle2 + " does not exist")
+        return ("FAILURE @" + handle1 + " could not follow @" + handle2).encode("utf-8")
+
+    for handle in followers[handle2]:
+        if(handle == handle1):
+            print(f"@{handle1} already follow @{handle2}")
+            return (f"FAILURE already follows @{handle2}").encode("utf-8")
 
     followers[handle2].append(handle1)
     followers[handle2].sort()
 
     follows[handle1].append(handle2)
 
-    output = "@" + handle1 + " followed @" + handle2
+    output = "SUCCESS @" + handle1 + " followed @" + handle2
     return output.encode("utf-8")
 
 def drop(input):
     # drop @<handle1> @<handle2>
+    # remove handle 1 from handle 2's followers
+    # remove handle 2 from handle 1's follows
     handle1 = inputs[1][1:]
     handle2 = inputs[2][1:]
+
+    if(not handle2 in users):
+        print(f"@{handle2} does not exist")
+        return (f"FAILURE @{handle2} does not exist").encode("utf-8")
+
+    found = False
+    for handle in followers[handle2]:
+        if (handle == handle1):
+            found = True
+            break
+
+    if(not found):
+        print(f"@{handle1} does not follow @{handle2}")
+        return (f"FAILURE @{handle2} is not followed").encode("utf-8")
 
     followers[handle2].remove(handle1)
     follows[handle1].remove(handle2)
 
-    output = "@" + handle1 + " unfollowed @" + handle2
+    output = "SUCCESS @" + handle1 + " unfollowed @" + handle2
     return output.encode("utf-8")
 
 def exitUser(input):
     # exit @<handle>
+    # remove user from any followers lists
+    # remove user from users list, and remove user from follows and folllowers
     handle = inputs[1][1:]
     for f in follows[handle]:
         followers[f].remove(handle)
+
+    for f in followers[handle]:
+        follows[f].remove(handle)
 
     del followers[handle]
     del follows[handle]
@@ -99,25 +144,27 @@ while True:
     print(inputs[0])
     reply = b""
 
-    if inputs[0] == "register":
+    # match message with correct input and build the reply message
+    if inputs[0] == "register" and len(inputs) == 6:
         reply = register(inputs)
 
-    elif inputs[0] == "query":
+    elif inputs[0] == "query" and inputs[1] == "handles":
         reply = query()
 
-    elif inputs[0] == "follow":
+    elif inputs[0] == "follow" and len(inputs) == 3:
         reply = follow(input)
 
-    elif inputs[0] == "drop":
+    elif inputs[0] == "drop" and len(inputs) == 3:
         reply = drop(input)
 
-    elif inputs[0] == "exit":
+    elif inputs[0] == "exit" and len(inputs) == 2:
         reply = exitUser(input)
 
     elif inputs[0] == "print":
         reply += query() + followss()
+
     else:
         reply = b"bad request"
 
-    # send reply
+    # send reply back
     sock.sendto(reply, (addr))
